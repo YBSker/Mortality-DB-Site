70ce958a49683ebee81d554c11ee5eacbed6c11a
YBSker <46542771+YBSker@users.noreply.github.com>
2020-05-03 10:25:55 -0400

    Sidebar functional/nicer looking. Links now all link to the right places. Might need PHP isntalled on local machine if running from there.

---
+++ main/index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="mainStyle.css">
+</head>
+
+<body>
+
+<?php
+include 'sidebar.html';
+?>
+<!--Div for Home filler text-->
+<div class="home fade-in">
+    <h2>Mortality or smth</h2>
+    </p>
+</div>
+
+
+
+<!-- Query to show raw score content (this is an html comment)  -->
+<!--	<h2> Show Raw Scores For One Student</h2>-->
+<!--	<form action="show_raw_scores.php" method="post">-->
+<!--		SSN: <input type="text" name="SSN"> <br/>-->
+<!--		<input type="submit">-->
+<!--	</form>-->
+<!--	<br/><br/>-->
+</body>
+</html>
+
---
+++ main/sub_pages/education.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../mainStyle.css">
+</head>
+
+<body>
+
+<?php
+include 'sub_page_sidebar.html';
+?>
+<!--Div for Home filler text-->
+<div class="home fade-in">
+    <h2>Mortality or smth</h2>
+    </p>
+</div>
+
+</body>
+</html>
