f151615079334af286d43e721dcedc4a403a2414
Stanley Chu <schu17@jhu.edu>
2020-05-08 15:11:35 -0400

    some updates...healthcare worked on.

--- main/health_CoD_viewGen.sql
+++ main/health_CoD_viewGen.sql
@@ -3,19 +3,21 @@ DROP VIEW IF EXISTS healthCare_perCapita_perCountry;
 DROP VIEW IF EXISTS average_deaths_per_country;
 DROP TABLE IF EXISTS perCountryDataCoDHealth;
 DROP VIEW IF EXISTS average_cardio;
+DROP VIEW IF EXISTS average_health_snap_per_country;
+DROP TABLE IF EXISTS perCountryDataCoDHealth_pre;
 
 DROP TABLE IF EXISTS average_cardio;
 
 
 CREATE VIEW healthCare_CoD_perCapita AS
-SELECT c.countryName, c.theYear, c.cardiovascular, c.homicide, c.roadInjury, hc.householdOutOfPocket, hc.capitalHealthExpend, hc.socialInsuranceContribution
+SELECT c.countryName, c.theYear, hc.countryIncomeGroup, c.cardiovascular, c.homicide, c.roadInjury, hc.householdOutOfPocket, hc.capitalHealthExpend, hc.socialInsuranceContribution
 FROM CausesOfDeathPercent AS c 
 INNER JOIN HealthCareExpenditureSnapshotPerCapita AS hc 
 ON hc.countryName = c.countryName 
 AND hc.year = c.theYear;
 
 CREATE VIEW healthCare_perCapita_perCountry AS
-SELECT a.countryName,
+SELECT a.countryName, countryIncomeGroup,
         AVG(NULLIF(householdOutOfPocket, 0)) as averageHouseholdOutOfPocket, 
         AVG(NULLIF(capitalHealthExpend, 0)) AS averageCapitalHealthExpend, 
         AVG(NULLIF(socialInsuranceContribution, 0)) AS averageSocialInsuranceContribution
@@ -30,8 +32,16 @@ SELECT c.countryName,
 FROM CausesOfDeathPercent AS c
 GROUP BY countryName;
 
-CREATE TABLE perCountryDataCoDHealth AS
-SELECT a.countryName, ROUND(averageHouseholdOutOfPocket, 2) AS averageHouseholdOutOfPocket, 
+CREATE VIEW average_health_snap_per_country AS
+SELECT countryName, 
+        AVG(NULLIF(lifeExpectancy, 0)) AS lifeExpectancy,
+        AVG(NULLIF(averageBMI, 0)) AS averageBMI
+FROM HealthCareQualitySnapshot
+GROUP BY countryName;
+
+CREATE TABLE perCountryDataCoDHealth_pre AS
+SELECT a.countryName, a.countryIncomeGroup,
+ ROUND(averageHouseholdOutOfPocket, 2) AS averageHouseholdOutOfPocket, 
 ROUND(averageCapitalHealthExpend, 2) AS averageCapitalHealthExpend, 
 ROUND(averageSocialInsuranceContribution, 2) AS averageSocialInsuranceContribution, 
 ROUND(average_cardiovascular, 2) AS average_cardiovascular, 
@@ -44,7 +54,18 @@ DROP VIEW IF EXISTS healthCare_CoD_perCapita;
 DROP VIEW IF EXISTS healthCare_perCapita_perCountry;
 DROP VIEW IF EXISTS average_deaths_per_country;
 
-CREATE TABLE average_cardio AS
-SELECT countryName, averageHouseholdOutOfPocket, average_cardiovascular 
-FROM perCountryDataCoDHealth ORDER BY averageHouseholdOutOfPocket DESC;
+CREATE TABLE perCountryDataCoDHealth AS
+SELECT a.countryName, a.countryIncomeGroup,
+ ROUND(averageHouseholdOutOfPocket, 2) AS averageHouseholdOutOfPocket, 
+ROUND(averageCapitalHealthExpend, 2) AS averageCapitalHealthExpend, 
+ROUND(averageSocialInsuranceContribution, 2) AS averageSocialInsuranceContribution, 
+ROUND(average_cardiovascular, 2) AS average_cardiovascular, 
+ROUND(average_homicide, 5) AS average_homicide, 
+ROUND(average_roadInjury, 5) AS average_roadInjury,
+ROUND(lifeExpectancy, 5) AS lifeExpectancy, 
+ROUND(averageBMI, 5) AS averageBMI
+FROM perCountryDataCoDHealth_pre AS a INNER JOIN average_health_snap_per_country AS c ON a.countryName LIKE c.countryName;
+
+DROP TABLE IF EXISTS perCountryDataCoDHealth_pre;
+DROP VIEW IF EXISTS average_health_snap_per_country;
 
--- main/sub_pages/healthcare.php
+++ main/sub_pages/healthcare.php
@@ -16,7 +16,17 @@ function outputResultsTableHeader()
     echo "<th> Rank of Deaths/158 <br>(1 is Least)</th>";
     echo "</tr>";
 }
-
+function outputResultsTableHeader_b()
+{
+    echo "<tr>";
+    echo "<th>  </th>";
+    echo "<th> Country </th>";
+    echo "<th> Cardiovascular Deaths in Percent of <br>Total Population </th>";
+    echo "<th> Homicide Deaths in Percent of <br>Total Population </th>";
+    echo "<th> Average Social Insurance Expenditures in Millions </th>";
+    echo "<th> Rank of Expenditure From Within Wealth Bracket <br>(1 is Most)</th>";
+    echo "</tr>";
+}
 include "../open.php";
 
 include 'sub_page_sidebar.html';
@@ -300,7 +310,6 @@ include 'sub_page_sidebar.html';
                         echo "0 results";
                     }
                     echo "</table>";
-                    $mysqli->close();
                     ?>
                 </div>
                 <h3>More social insurance contributions seemed to correspond to higher cardiovascular disease death,
@@ -316,20 +325,186 @@ include 'sub_page_sidebar.html';
                     treatments for homicides and vehicular accidents. </h3>
             </div>
             <div class="general fade-in tab_me">
-                <h3>Overall, based purely on our data sources, it would seem that more spending by a country and households on healthcare corresponds
-                to a higher rate of cardiovascular death. The country data would seem to be skewed, as there is some heavy error with specific countries such
-                as Japan and France. Data on households seems to be consistent with other studies...particularly in the case of affording preventative medications.
-                More spending in general seemed to be indicative of a lower risk of death by homicide and vehicular accidents. Public spending on infrastructure and
-                individual healthcare makes sense, as there would be more accessability to life saving treatments. Individual spendings may be indicative of more availability
-                to spend money on healthcare and again be indicative of more accessability to life saving treatments or hospitals in drastic cases.</h3>
+                <h3>Overall, based purely on our data sources, it would seem that more spending by a country and
+                    households on healthcare corresponds
+                    to a higher rate of cardiovascular death. The country data would seem to be skewed, as there is some
+                    heavy error with specific countries such
+                    as Japan and France. Data on households seems to be consistent with other studies...particularly in
+                    the case of affording preventative medications.
+                    More spending in general seemed to be indicative of a lower risk of death by homicide and vehicular
+                    accidents. Public spending on infrastructure and
+                    individual healthcare makes sense, as there would be more accessibility to life saving treatments.
+                    Individual spendings may be indicative of more availability
+                    to spend money on healthcare and again be indicative of more accessibility to life saving treatments
+                    or hospitals in drastic cases.</h3>
+                <h3>Click above to get data and analysis on specific sections!</h3>
+            </div>
+
+            <h1 style="color: #87CEFA;">Wealth of a Country and Relation to the Cost of Death</h1>
+
+            <div class="general fade-in tab_me">
+                <h3>In this section, we attempt to look at three general levels of a country's wealth- low, mid, and
+                    high based on data from the WHO.
+                    We will be looking at a country's costs on affording healthcare compared to the rates of death
+                    for cardiovascular disease. This indicator is chosen
+                    as it is the number one cause of death worldwide.</h3>
+            </div>
+
+            <button type="button" class="collapsible">Top 10 Cardiovascular Death Rates of High Income Countries
+            </button>
+            <div class="content general fade-in tab_me">
+                <h2>High Income Death Rates and Social Healthcare Spending</h2>
+                <div class="general fade-in">
+                    <?php
+                    $sql = "SELECT countryName, average_cardiovascular, average_homicide, averageSocialInsuranceContribution, RANK() OVER (ORDER BY averageSocialInsuranceContribution) spending_rank FROM perCountryDataCoDHealth WHERE countryIncomeGroup = 'Hi' AND averageSocialInsuranceContribution IS NOT NULL  ORDER BY average_cardiovascular DESC";
+                    $result = $mysqli->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo "<table border=\"1px solid black\">";
+                        outputResultsTableHeader_b();
+                        for ($i = 1; $i < 11; $i++) {
+                            $row = $result->fetch_assoc();
+                            echo "<tr>";
+                            echo "<td>$i</td>";
+                            echo "<td>" . $row["countryName"] . "</td>";
+                            echo "<td>" . $row["average_cardiovascular"] . "</td>";
+                            echo "<td>" . $row["average_homicide"] . "</td>";
+                            echo "<td>" . $row["averageSocialInsuranceContribution"] . "</td>";
+                            echo "<td>" . $row["spending_rank"] . "</td>";
+                            echo "</tr>";
+
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    echo "</table>";
+                    ?>
+                </div>
+                <h3>A higher out of pocket cost for households in affording healthcare seemed to correspond with a
+                    higher rate of deaths related to cardiovascular disease,
+                    a lower rate of deaths related to homicide, and a lower rate of death to do with vehicular
+                    accidents. The case of cardiovascular disease seems to be sensible, as
+                    higher out of pocket costs are perhaps more indicative of inability to afford preventative
+                    medications or treatments. In the cases of homicides and vehicular accidents,
+                    perhaps there are more factors at play, as there does seem to be a range of rankings that a
+                    country
+                    can take. Perhaps we need to look at more statistics.</h3>
+            </div>
+            <button type="button" class="collapsible">Top 10 Cardiovascular Death Rates of Middle Income Countries
+            </button>
+            <div class="content general fade-in tab_me">
+                <h2>Middle Income Death Rates and Social Healthcare Spending</h2>
+                <div class="general fade-in">
+                    <?php
+                    $sql = "SELECT countryName, average_cardiovascular, average_homicide, averageSocialInsuranceContribution, RANK() OVER (ORDER BY averageSocialInsuranceContribution DESC) spending_rank FROM perCountryDataCoDHealth WHERE (countryIncomeGroup = 'Low-Mid' OR countryIncomeGroup = 'Up-Mid') AND averageSocialInsuranceContribution IS NOT NULL  ORDER BY average_cardiovascular DESC";
+                    $result = $mysqli->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo "<table border=\"1px solid black\">";
+                        outputResultsTableHeader_b();
+                        for ($i = 1; $i < 11; $i++) {
+                            $row = $result->fetch_assoc();
+                            echo "<tr>";
+                            echo "<td>$i</td>";
+                            echo "<td>" . $row["countryName"] . "</td>";
+                            echo "<td>" . $row["average_cardiovascular"] . "</td>";
+                            echo "<td>" . $row["average_homicide"] . "</td>";
+                            echo "<td>" . $row["averageSocialInsuranceContribution"] . "</td>";
+                            echo "<td>" . $row["spending_rank"] . "</td>";
+                            echo "</tr>";
+
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    echo "</table>";
+                    ?>
+                </div>
+                <h3>More capital health expenditures (investments in health infrastructure, vaccine stocks, etc)
+                    seemed
+                    to correspond with higher rates of
+                    cardiovascular related deaths and lower homicide and vehicular accident death rates with a
+                    couple of
+                    outliers. For cardiovascular disease related deaths,
+                    perhaps infrastructure is not indicative of chances of surviving heart attacks and is more of an
+                    indicator of a nation's wealth. This could be indicative of longer
+                    life spans or other factors leading to higher rates of cardiovascular disease. Infrastructure
+                    might
+                    not be as helpful in the case of cardiovascular disease. For instance, one of
+                    the biggest indicators of surviving a heart attack is time until CPR or defibrillation. As for
+                    lower
+                    homicide and vehicular accident deaths, in general data seems to trend towards
+                    lower likelihood of this fatality. Likely since these two forms of death rely more on likelihood
+                    and
+                    speed of treatment over a longer term and with more specific equipment.
+                    The two outliers are consistent between both homicide and vehicular accidents and perhaps can be
+                    considered as unrepresentative outliers.</h3>
+            </div>
+            <button type="button" class="collapsible">Top 10 Cardiovascular Death Rates of Low Income Countries
+            </button>
+            <div class="content general fade-in tab_me">
+                <h2>Low Income Death Rates and Social Healthcare Spending</h2>
+                <div class="general fade-in">
+                    <?php
+                    $sql = "SELECT countryName, average_cardiovascular, average_homicide, averageSocialInsuranceContribution, RANK() OVER (ORDER BY averageSocialInsuranceContribution) spending_rank FROM perCountryDataCoDHealth WHERE countryIncomeGroup = 'Low' AND averageSocialInsuranceContribution IS NOT NULL  ORDER BY average_cardiovascular DESC";
+                    $result = $mysqli->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo "<table border=\"1px solid black\">";
+                        outputResultsTableHeader_b();
+                        for ($i = 1; $i < 11; $i++) {
+                            $row = $result->fetch_assoc();
+                            echo "<tr>";
+                            echo "<td>$i</td>";
+                            echo "<td>" . $row["countryName"] . "</td>";
+                            echo "<td>" . $row["average_cardiovascular"] . "</td>";
+                            echo "<td>" . $row["average_homicide"] . "</td>";
+                            echo "<td>" . $row["averageSocialInsuranceContribution"] . "</td>";
+                            echo "<td>" . $row["spending_rank"] . "</td>";
+                            echo "</tr>";
+
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    echo "</table>";
+                    $mysqli->close();
+                    ?>
+                </div>
+                <h3>More social insurance contributions seemed to correspond to higher cardiovascular disease death,
+                    lower homicide death rates, and lower vehicular accident death rates.
+                    As for the cardiovascular death rates, this may be indicative of an error in our data. Other
+                    studies
+                    have demonstrated that countries such as France and Japan have
+                    some of the LOWEST rates of death by cardiovascular disease
+                    (https://www.aihw.gov.au/getmedia/a8138409-090c-482e-8e5f-e64943be8ff0/hsvd-c06.pdf.aspx). As
+                    for
+                    the rest,
+                    homicide and vehicular death rates seem consistent other than the consistent outlier of the
+                    United
+                    States. Higher social insurance contributions would be indicative of being
+                    able to spend preventative medications in the case of cardiovascular disease and to afford
+                    emergency
+                    treatments for homicides and vehicular accidents. </h3>
+            </div>
+            <div class="general fade-in tab_me">
+                <h3>Overall, based purely on our data sources, it would seem that more spending by a country and
+                    households on healthcare corresponds
+                    to a higher rate of cardiovascular death. The country data would seem to be skewed, as there is
+                    some heavy error with specific countries such
+                    as Japan and France. Data on households seems to be consistent with other studies...particularly
+                    in the case of affording preventative medications.
+                    More spending in general seemed to be indicative of a lower risk of death by homicide and
+                    vehicular accidents. Public spending on infrastructure and
+                    individual healthcare makes sense, as there would be more accessability to life saving
+                    treatments. Individual spendings may be indicative of more availability
+                    to spend money on healthcare and again be indicative of more accessability to life saving
+                    treatments or hospitals in drastic cases.</h3>
                 <h3>Click above to get data and analysis on specific sections!</h3>
             </div>
 
         </div>
     </div>
-</div>
 
-<script src="sub_page_scripts.js"></script>
+
+    <script src="sub_page_scripts.js"></script>
 
 </body>
 </html>
