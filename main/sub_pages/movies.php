5127ed3962300e9b95c0d209bd808264d7b0f549
Jerry Chen <jchen268@jhu.edu>
2020-05-10 18:31:30 -0400

    copied CoD section to Movies

--- main/sub_pages/movies.php
+++ main/sub_pages/movies.php
@@ -27,6 +27,18 @@ function outputResultsTableHeader_b()
     echo "</tr>";
 }
 
+function outputResultsTableHeader_c()
+{
+    echo "<tr>";
+    echo "<th>  </th>";
+    echo "<th> Country </th>";
+    echo "<th> Total Movies</th>";
+    echo "<th> Total Action Movies</th>";
+    echo "<th> Rate of Action Movies</th>";
+    echo "<th> Rate of Homicide as Cause of Death</th>";
+    echo "</tr>";
+}
+
 include "../open.php";
 
 include 'sub_page_sidebar.html';
@@ -40,7 +52,7 @@ include 'sub_page_sidebar.html';
 
     <div class="image-container2">
         <div class="sub_page_spacing">
-            <h1 class="tab_title" style="color: #87CEFA;">Movie Preferences and Life Expectancy</h1>
+            <h1 style="color: #87CEFA;">Movie Preferences and Life Expectancy</h1>
             <div class="general fade-in tab_me">
                 <h3>In this section, we look at whether or not a country's movie preferences has any correlation
                     with the country's average life expectancy. Data in this section
@@ -295,7 +307,7 @@ include 'sub_page_sidebar.html';
 
             </div>
 
-            <h1 class="tab_title" style="color: #87CEFA;">Movie Ratings and Happiness</h1>
+            <h1 style="color: #87CEFA;">Movie Ratings and Happiness</h1>
 
             <div class="general fade-in tab_me">
                 <h3>In this section, we look at whether or not there's a correlation between average movie ratings for a
@@ -379,8 +391,101 @@ include 'sub_page_sidebar.html';
 
             </div>
 
+            <h1 style="color: #87CEFA;">Action Movies and Homicide</h1>
+            <div class="general fade-in tab_me">
+                <h3>In this section, we look at whether or not the proportion of action movies to total movies a country produces
+                    is correlated with average proportion of deaths in that country that are categorized as homicide, as action is the genre
+                    with the highest frequency of violent scenes.
+                    Data in this section is and collected from <a href="https://www.kaggle.com/danielgrijalvas/movies"> IMDb </a>and <a
+                            href="https://ourworldindata.org/homicides"> ourworldindata.org.<a/></h3>
+            </div>
+
+            <button type="button" class="collapsible">Do Action Movies Make People Violent?
+            </button>
+            <div class="content general fade-in tab_me">
+                <h2>Top 10 Countries By Action Movie Production</h2>
+                <div class="general fade-in">
+                    <?php
+                    $sql = "SELECT country, 
+                            COUNT(*) AS num, 
+                            sum(case when genre = 'Action' then 1 else 0 end) AS com,
+                            sum(case when genre = 'Action' then 1 else 0 end) / COUNT(*) AS rate,    
+                            average_homicide AS hom
+                            FROM Movies
+                            INNER JOIN perCountryDataCoDHealth ON perCountryDataCoDHealth.countryName = Movies.country
+                            GROUP BY country
+                            HAVING COUNT(*) > 9
+                            ORDER BY hom DESC                            
+                            ";
+                    $result = $mysqli->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo "<table border=\"1px solid black\">";
+                        outputResultsTableHeader_c();
+                        for ($i = 1; $i < 11; $i++) {
+                            $row = $result->fetch_assoc();
+                            echo "<tr>";
+                            echo "<td>$i</td>";
+                            echo "<td>" . $row["country"] . "</td>";
+                            echo "<td>" . $row["num"] . "</td>";
+                            echo "<td>" . $row["com"] . "</td>";
+                            echo "<td>" . $row["rate"] . "</td>";
+                            echo "<td>" . $row["hom"] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    echo "</table>";
+                    ?>
+                </div>
+                <h2>Bottom 10 Countries by Action Movie Production</h2>
+                <div class="general fade-in">
+                    <?php
+                    $sql = "SELECT country, 
+                            COUNT(*) AS num, 
+                            sum(case when genre = 'Action' then 1 else 0 end) AS com,
+                            sum(case when genre = 'Action' then 1 else 0 end) / COUNT(*) AS rate,    
+                            average_homicide AS hom
+                            FROM Movies
+                            INNER JOIN perCountryDataCoDHealth ON perCountryDataCoDHealth.countryName = Movies.country
+                            GROUP BY country
+                            HAVING COUNT(*) > 9
+                            ORDER BY hom ASC                            
+                            ";
+                    $result = $mysqli->query($sql);
+                    if ($result->num_rows > 0) {
+                        echo "<table border=\"1px solid black\">";
+                        outputResultsTableHeader_c();
+                        for ($i = 1; $i < 11; $i++) {
+                            $row = $result->fetch_assoc();
+                            echo "<tr>";
+                            echo "<td>$i</td>";
+                            echo "<td>" . $row["country"] . "</td>";
+                            echo "<td>" . $row["num"] . "</td>";
+                            echo "<td>" . $row["com"] . "</td>";
+                            echo "<td>" . $row["rate"] . "</td>";
+                            echo "<td>" . $row["hom"] . "</td>";
+                            echo "</tr>";
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                    echo "</table>";
+                    ?>
+                </div>
+            </div>
+            <div class="general fade-in tab_me">
+                <h3>We looked
+                    at top 10 and bottom 10 countries by proportion of total movies produced that are action movies. Just from the
+                    eyeball test, it's hard to see any meaningful pattern, but the countries producing proportionately less action movies
+                    do seem to have a lower rate of homicide, though you should take that with a grain of salt without rigorous statistical
+                    analysis to back it up.<br/>
+
+                </h3>
+                <h3>Click above to get data and analysis on specific sections!</h3>
+
+            </div>
         </div>
-    </div>
 
 
     <script src="sub_page_scripts.js"></script>
